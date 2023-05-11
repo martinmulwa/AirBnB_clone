b122069dcfddb289274e8252d2a6cc0ff9b4fffd
Martin Pius <martinmulwa1711@gmail.com>
2023-05-11 18:13:13 +0300

    chore: Create BaseModel Class specification

---
+++ models/base_model.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Base Model module."""
+
+import datetime
+import uuid
+
+
+class BaseModel:
+    """Defines all common attributes/methods for other classes."""
+
+    def __init__(self):
+        """Initilize a BaseModel object with given attributes."""
+        self.id = str(uuid.uuid4())
+
+        now = datetime.datetime.now()
+        self.created_at = now
+
+        # create copy of now datetime object
+        self.updated_at = datetime.datetime(
+            now.year,
+            now.month,
+            now.day,
+            now.hour,
+            now.minute,
+            now.second,
+            now.microsecond,
+        )
+
+    def __str__(self):
+        """Return unofficial string representation of a BaseModel object"""
+        class_name = self.__class__.__name__
+
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary containing attributes of BaseModel object."""
+        bm_dict = self.__dict__
+
+        class_name = self.__class__.__name__
+        created_at = self.created_at.isoformat()
+        updated_at = self.updated_at.isoformat()
+
+        bm_dict["__class__"] = class_name
+        bm_dict["created_at"] = created_at
+        bm_dict["updated_at"] = updated_at
+
+        return bm_dict
