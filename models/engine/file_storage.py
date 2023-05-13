214941ace1154813579df6c0220e1e8f3613b0b0
Martin Pius <martinmulwa1711@gmail.com>
2023-05-13 15:35:27 +0300

    feat: Implement 'new' method of FileStorage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,3 +11,8 @@ class FileStorage:
     def all(self):
         """Return dictionary of all stored objects."""
         return self.__objects
+
+    def new(self, obj):
+        """Add obj to __objects with key <obj class name>.id."""
+        obj_key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[obj_key] = obj
