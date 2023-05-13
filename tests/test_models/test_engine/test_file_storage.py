33031187ebd5f376213c325c0b67d083ab8c14f7
Martin Pius <martinmulwa1711@gmail.com>
2023-05-13 15:31:47 +0300

    test: Add tests for 'new' method of the FileStorage class

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,6 +2,7 @@
 """Test FileStorage module."""
 
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 import unittest
 
 
@@ -19,6 +20,20 @@ class TestFileStorage(unittest.TestCase):
         self.assertIsInstance(FileStorage._FileStorage__objects, dict)
 
     def test_all(self):
-        """Test all method of FileStorage objects."""
+        """Test 'all' method of FileStorage objects."""
         fs = FileStorage()
         self.assertIs(fs.all(), FileStorage._FileStorage__objects)
+
+    def test_new(self):
+        """Test 'new' method of FileStorage objects."""
+        fs = FileStorage()
+
+        # create new BaseModel object and check that it's not in __objects
+        bm = BaseModel()
+        bm_key = f"{bm.__class__.__name__}.{bm.id}"
+        self.assertTrue(bm_key not in FileStorage._FileStorage__objects)
+
+        # add the BaseModel object to __objects and check that it was added
+        fs.new(bm)
+        self.assertTrue(bm_key in FileStorage._FileStorage__objects)
+        self.assertIs(bm, FileStorage._FileStorage__objects[bm_key])
