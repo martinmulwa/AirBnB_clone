8974be2c45f1c6527fee07ff7d193370feda3545
Martin Pius <martinmulwa1711@gmail.com>
2023-05-13 14:54:26 +0300

    test: Add tests for FileStorage class

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -7,3 +7,13 @@ import unittest
 
 class TestFileStorage(unittest.TestCase):
     """Test methods and attributes in FileStorage class."""
+
+    def test_init(self):
+        """Test that FileStorage objects are initialized correctly."""
+        # check that FileStorage has __file_path private class attribute
+        self.assertIsNotNone(FileStorage._FileStorage__file_path)
+        self.assertIsInstance(FileStorage._FileStorage__file_path, str)
+
+        # check that FileStorage has __objects private class attribute
+        self.assertIsNotNone(FileStorage._FileStorage__objects)
+        self.assertIsInstance(FileStorage._FileStorage__objects, dict)
