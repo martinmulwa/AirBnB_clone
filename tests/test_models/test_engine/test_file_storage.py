12658dd374c65a362800286021a0c286f9294925
Martin Pius <martinmulwa1711@gmail.com>
2023-05-13 14:31:18 +0300

    test: Create tests for FileStorage class

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Test FileStorage module."""
+
+from models.engine.file_storage import FileStorage
+import unittest
+
+
+class TestFileStorage(unittest.TestCase):
+    """Test methods and attributes in FileStorage class."""
