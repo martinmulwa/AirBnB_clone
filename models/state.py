9e3d43b13991754f5ac772b89cdf0ccced46e7d3
Martin Pius <martinmulwa1711@gmail.com>
2023-05-14 17:51:23 +0300

    feat: Add State class

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""State module."""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Defines attributes and methods for State objects."""
+
+    name = ""
