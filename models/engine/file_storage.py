ee66af1707b0781bb87e1ac252669198b44937e1
Martin Pius <martinmulwa1711@gmail.com>
2023-05-14 19:32:01 +0300

    feat: Update FileStorage to work with REview objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -19,6 +19,7 @@ class FileStorage:
         from models.city import City
         from models.amenity import Amenity
         from models.place import Place
+        from models.review import Review
 
         classes = {
             "BaseModel": BaseModel,
@@ -26,7 +27,8 @@ class FileStorage:
             "State": State,
             "City": City,
             "Amenity": Amenity,
-            "Place": Place
+            "Place": Place,
+            "Review": Review
         }
 
         return classes.get(class_name)
