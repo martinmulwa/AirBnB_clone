6f9d9fed4b19afd922b26174239e4eea83254a62
Martin Pius <martinmulwa1711@gmail.com>
2023-05-14 12:33:15 +0300

    fix: Update FileStorage class to avoid circular import error

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,26 +2,34 @@
 """FileStorage module."""
 
 import json
-from models.base_model import BaseModel
 
 
 class FileStorage:
-    """Implements a simple file storage."""
+    """Implements simple file storage."""
 
     __file_path = "file.json"
     __objects = {}
+
+    @staticmethod
+    def get_constructor(class_name):
+        """Return a valid class constructor given a valid class name."""
+        from models.base_model import BaseModel
+
+        classes = {"BaseModel": BaseModel}
+
+        return classes.get(class_name)
 
     def all(self):
         """Return dictionary of all stored objects."""
         return self.__objects
 
     def new(self, obj):
-        """Add obj to __objects with key <obj class name>.id."""
+        """Add new obj to __objects with key <obj class name>.id."""
         obj_key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[obj_key] = obj
 
     def save(self):
-        """Serializes __objects to JSON file."""
+        """Serialize __objects to JSON file."""
         objects_serialized = {}
 
         # ensure every object in __objects can be serialized
@@ -39,10 +47,12 @@ class FileStorage:
             raise Exception(f"Error: Could not write to {self.__file_path}")
 
     def reload(self):
-        """Deserializes JSON file to __objects."""
+        """Deserializes the JSON file to __objects."""
         try:
             # remove all objects currently in memory
             self.__objects.clear()
+
+            objects_loaded = {}
 
             # read content of file_path
             try:
@@ -53,9 +63,13 @@ class FileStorage:
 
             # deserialize the objects in objects_loaded
             for obj_key, obj_dict in objects_loaded.items():
-                class_name = globals()[obj_dict["__class__"]]
-                obj = class_name(**obj_dict)
-                self.__objects[obj_key] = obj
+                class_name = obj_dict.get("__class__")
+                constructor = self.get_constructor(class_name)
+
+                if constructor is None:
+                    raise Exception(f"Error: Invalid class name: {class_name}")
+
+                self.__objects[obj_key] = constructor(**obj_dict)
 
         except FileNotFoundError:
             pass
