4d6a7ce4b75fb5b7a7b1ed65d86451274a353f2c
Martin Pius <martinmulwa1711@gmail.com>
2023-05-14 18:29:14 +0300

    feat: Add Amenity class

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Amenity module."""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Defines attributes and methods for Amenity objects."""
+
+    name = ""
