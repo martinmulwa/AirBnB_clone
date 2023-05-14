8bfaf8194725298813a08f24ede34b7a148341a3
Martin Pius <martinmulwa1711@gmail.com>
2023-05-14 18:20:12 +0300

    feat: Add City class

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""City module."""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Defines attributes and methods for City objects."""
+
+    state_id = ""
+    name = ""
